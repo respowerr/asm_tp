ec83e06f4d5311f9085927c6f7684429b04dc870
respowerr <respowerr@callidosgroup.eu>
2025-09-30 08:53:39 +0200

    asm07

---
+++ asm07/asm07.s
@@ -0,0 +1,53 @@
+section .bss
+    buffer resb 32
+
+section .text
+    global _start
+
+_start:
+    mov rax, 0
+    mov rdi, 0
+    mov rsi, buffer
+    mov rdx, 32
+    syscall
+
+    xor rbx, rbx
+    mov rsi, buffer
+.convert:
+    mov al, [rsi]
+    cmp al, 10
+    je .done_convert
+    sub al, '0'
+    imul rbx, rbx, 10
+    add rbx, rax
+    inc rsi
+    jmp .convert
+.done_convert:
+
+    cmp rbx, 2
+    jb not_prime
+
+    mov rcx, 2
+.check_loop:
+    mov rax, rbx
+    xor rdx, rdx
+    div rcx
+    cmp rdx, 0
+    je .divisible
+
+    inc rcx
+    mov rax, rcx
+    imul rax, rax
+    cmp rax, rbx
+    jbe .check_loop
+
+    ; --> alors nombre premier
+    mov rax, 60
+    xor rdi, rdi
+    syscall
+
+.divisible:
+not_prime:
+    mov rax, 60
+    mov rdi, 1
+    syscall
