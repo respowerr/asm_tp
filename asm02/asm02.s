e49bfd081ae820876b3af868374a591273484493
respowerr <respowerr@callidosgroup.eu>
2025-10-03 15:21:27 +0200

    Adapte for new test

--- asm02/asm02.s
+++ asm02/asm02.s
@@ -6,7 +6,7 @@ section .data
     msglen equ $ - msg
 
 section .bss
-    buf resb 8
+    buf resb 64
 
 section .text
     global _start
@@ -15,9 +15,29 @@ _start:
     mov rax, 0
     mov rdi, 0
     mov rsi, buf
-    mov rdx, 8
+    mov rdx, 64
     syscall
     mov rbx, rax
+
+    mov rcx, rbx
+    mov rdi, buf
+.find_nl:
+    cmp rcx, 0
+    je .no_newline
+    mov al, byte [rdi]
+    cmp al, 0x0A
+    je .truncate
+    inc rdi
+    dec rcx
+    jmp .find_nl
+
+.truncate:
+    mov byte [rdi+1], 0
+    mov rbx, rdi
+    sub rbx, buf
+    add rbx, 1
+
+.no_newline:
 
     mov rcx, goodlen
     cmp rbx, rcx
